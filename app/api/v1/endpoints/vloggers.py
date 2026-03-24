af5c7fece8cd470a529d74cda46b9fc394db15d9
Vladut Cretu <vladutcretu98@gmail.com>
2026-03-24 12:52:31 +0200

    ✨ Implement endpoint PATCH /api/v1/vloggers/{vlogger_id} (#8)

--- app/api/v1/endpoints/vloggers.py
+++ app/api/v1/endpoints/vloggers.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, status, HTTPException
 
-from app.schemas.vlogger import VloggerResponse, VloggerCreate
+from app.schemas.vlogger import VloggerResponse, VloggerCreate, VloggerUpdate
 from app.api.dependencies import CurrentUser, DatabaseSession
 from app.repositories.vloggers import VloggersRepository, VloggerAlreadyExistsError
-from app.services.vloggers import VloggersService
+from app.services.vloggers import VloggersService, VloggerDoesntExistError
 
 
 router = APIRouter(prefix="/vloggers", tags=["Vloggers"])
@@ -29,3 +29,31 @@ async def create_vlogger(
         )
 
     return vlogger
+
+
+@router.patch(
+    "/{vlogger_id}", response_model=VloggerResponse, status_code=status.HTTP_200_OK
+)
+async def update_vlogger(
+    vlogger_id: int,
+    vlogger_data: VloggerUpdate,
+    current_user: CurrentUser,
+    db: DatabaseSession,
+):
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized"
+        )
+
+    repository = VloggersRepository(db)
+    service = VloggersService(repository)
+
+    try:
+        vlogger = await service.get_vlogger_by_id(vlogger_id)
+    except VloggerDoesntExistError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Vlogger does not exist"
+        )
+
+    updated_vlogger = await service.update_vlogger(vlogger, vlogger_data)
+    return updated_vlogger
--- tests/api/v1/test_vloggers.py
+++ tests/api/v1/test_vloggers.py
@@ -160,3 +160,154 @@ async def test_post_vloggers_endpoint_duplicate_channel_url(
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json()["detail"] == "Vlogger already exists"
+
+
+# Endpoint PATCH /api/v1/vloggers/{vlogger_id}
+async def test_patch_vloggers_endpoint_success(vlogger, client, admin_token):
+    assert vlogger.id
+    assert vlogger.youtube_channel_id == "test_channel_id"
+    assert vlogger.youtube_channel_name == "test_channel_name"
+    assert vlogger.youtube_channel_url == "test_channel_url"
+    assert vlogger.youtube_avatar_url == "test_avatar_url"
+
+    response = await client.patch(
+        f"/api/v1/vloggers/{vlogger.id}",
+        headers={"Authorization": f"Bearer {admin_token}"},
+        json={
+            "youtube_channel_name": "test_name",
+            "youtube_channel_url": "test_url",
+            "youtube_avatar_url": "test_avatar",
+        },
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    assert data["youtube_channel_id"] == "test_channel_id"
+    assert data["youtube_channel_name"] == "test_name"
+    assert data["youtube_channel_url"] == "test_url"
+    assert data["youtube_avatar_url"] == "test_avatar"
+    assert data["id"] == vlogger.id
+    assert "created_at" in data
+
+
+async def test_patch_vloggers_endpoint_as_superuser(vlogger, client, superuser_token):
+    assert vlogger.id
+
+    response = await client.patch(
+        f"/api/v1/vloggers/{vlogger.id}",
+        headers={"Authorization": f"Bearer {superuser_token}"},
+        json={
+            "youtube_channel_id": "test_id",
+            "youtube_channel_name": "test_name",
+            "youtube_channel_url": "test_url",
+            "youtube_avatar_url": "test_avatar",
+        },
+    )
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.json()["detail"] == "Not authorized"
+
+
+async def test_patch_vloggers_endpoint_as_user(vlogger, client, user_token):
+    assert vlogger.id
+
+    response = await client.patch(
+        f"/api/v1/vloggers/{vlogger.id}",
+        headers={"Authorization": f"Bearer {user_token}"},
+        json={
+            "youtube_channel_id": "test_id",
+            "youtube_channel_name": "test_name",
+            "youtube_channel_url": "test_url",
+            "youtube_avatar_url": "test_avatar",
+        },
+    )
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.json()["detail"] == "Not authorized"
+
+
+async def test_patch_vloggers_endpoint_without_token(vlogger, client):
+    assert vlogger.id
+
+    response = await client.patch(
+        f"/api/v1/vloggers/{vlogger.id}",
+        headers={"Authorization": "Bearer "},
+        json={
+            "youtube_channel_id": "test_id",
+            "youtube_channel_name": "test_name",
+            "youtube_channel_url": "test_url",
+            "youtube_avatar_url": "test_avatar",
+        },
+    )
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json()["detail"] == "Not authenticated"
+
+
+async def test_patch_vloggers_endpoint_invalid_vlogger(client, admin_token):
+    response = await client.patch(
+        "/api/v1/vloggers/325654",
+        headers={"Authorization": f"Bearer {admin_token}"},
+        json={
+            "youtube_channel_id": "test_id",
+            "youtube_channel_name": "test_name",
+            "youtube_channel_url": "test_url",
+            "youtube_avatar_url": "test_avatar",
+        },
+    )
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json()["detail"] == "Vlogger does not exist"
+
+
+async def test_patch_vloggers_endpoint_single_field(vlogger, client, admin_token):
+    assert vlogger.id
+    assert vlogger.youtube_channel_name == "test_channel_name"
+    assert vlogger.youtube_channel_url == "test_channel_url"
+    assert vlogger.youtube_avatar_url == "test_avatar_url"
+
+    response = await client.patch(
+        f"/api/v1/vloggers/{vlogger.id}",
+        headers={"Authorization": f"Bearer {admin_token}"},
+        json={"youtube_channel_name": "test_name"},
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    assert data["youtube_channel_name"] == "test_name"
+    assert data["youtube_channel_url"] == "test_channel_url"
+    assert data["youtube_avatar_url"] == "test_avatar_url"
+    assert data["id"] == vlogger.id
+
+
+async def test_patch_vloggers_endpoint_invalid_field(vlogger, client, admin_token):
+    assert vlogger.id
+    assert vlogger.youtube_channel_id == "test_channel_id"
+
+    response = await client.patch(
+        f"/api/v1/vloggers/{vlogger.id}",
+        headers={"Authorization": f"Bearer {admin_token}"},
+        json={"youtube_channel_id": "test_id"},
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    assert data["youtube_channel_id"] == "test_channel_id"
+    assert data["id"] == vlogger.id
+    assert "created_at" in data
+
+
+async def test_patch_vloggers_endpoint_invalid_type(vlogger, client, admin_token):
+    assert vlogger.id
+    assert vlogger.youtube_channel_id == "test_channel_id"
+
+    response = await client.patch(
+        f"/api/v1/vloggers/{vlogger.id}",
+        headers={"Authorization": f"Bearer {admin_token}"},
+        json={"youtube_channel_name": 24},
+    )
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
