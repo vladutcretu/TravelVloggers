68d41812387f56329680a54a229a64b14b378482
Vladut Cretu <vladutcretu98@gmail.com>
2026-03-27 16:33:32 +0200

    Create Vlog model with FK & relationship Vlogger, Country and update Vlogger, Country models

--- app/models/vlog.py
+++ app/models/vlog.py
@@ -1,12 +1,59 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, ForeignKey, DateTime, func, Index
+from pydantic import computed_field
 
 from app.db.connection import Base
+from app.models.vlogger import Vlogger
 
 
 class Country(Base):
     __tablename__ = "countries"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
     name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     iso_code: Mapped[str] = mapped_column(String(2), unique=True, nullable=False)
+
+    vlogs: Mapped[list["Vlog"]] = relationship(
+        back_populates="country", cascade="all, delete-orphan"
+    )
+
+
+class Vlog(Base):
+    __tablename__ = "vlogs"
+    __table_args__ = Index("ix_vlogs_vlogger_country", "vlogger_id", "country_id")
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    vlogger_id: Mapped[int] = mapped_column(
+        ForeignKey("vloggers.id", ondelete="CASCADE"), nullable=False, index=True
+    )
+    country_id: Mapped[int] = mapped_column(
+        ForeignKey("countries.id", ondelete="CASCADE"), nullable=False, index=True
+    )
+
+    youtube_video_id: Mapped[str] = mapped_column(
+        String(11),
+        unique=True,
+        nullable=False,
+    )
+    published_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False
+    )
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    thumbnail_url: Mapped[str] = mapped_column(String(255), nullable=False)
+    language: Mapped[str] = mapped_column(String(2), nullable=True)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+    vlogger: Mapped[Vlogger] = relationship(back_populates="vlogs")
+    country: Mapped[Country] = relationship(back_populates="vlogs")
+
+    @computed_field
+    @property
+    def youtube_video_url(self) -> str:
+        return f"https://www.youtube.com/watch?v={self.youtube_video_id}"
--- app/models/vlogger.py
+++ app/models/vlogger.py
@@ -1,15 +1,17 @@
 from datetime import datetime
 
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, DateTime, func
 
 from app.db.connection import Base
+from app.models.vlog import Vlog
 
 
 class Vlogger(Base):
     __tablename__ = "vloggers"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
     youtube_channel_id: Mapped[str] = mapped_column(
         String(255), unique=True, nullable=False, index=True
     )
@@ -20,6 +22,11 @@ class Vlogger(Base):
         String(255), unique=True, nullable=False
     )
     youtube_avatar_url: Mapped[str] = mapped_column(String(255), nullable=False)
+
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
+    )
+
+    vlogs: Mapped[list[Vlog]] = relationship(
+        back_populates="vlogger", cascade="all, delete-orphan"
     )
