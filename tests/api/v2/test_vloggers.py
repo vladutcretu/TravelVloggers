51b41d11756f762302cdf69d9ac8614af2b224c3
Vladut Cretu <vladutcretu98@gmail.com>
2026-05-25 15:30:29 +0300

    Write tests

--- tests/api/v2/test_vloggers.py
+++ tests/api/v2/test_vloggers.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 from fastapi import status
 
@@ -82,6 +82,78 @@ async def test_get_vlogger_countries_endpoint_invalid_vlogger(client):
 
 async def test_get_vlogger_countries_endpoint_invalid_type(client):
     response = await client.get("/api/v2/vloggers/invalid/countries")
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+    assert response.json()["detail"][0]["type"] == "int_parsing"
+
+
+# Endpoint GET /api/v2/vloggers/{vlogger_id}/country/{country_id}
+async def test_get_vlogger_vlogs_by_country_endpoint_success(
+    vlogger, client, db_session, countries_factory
+):
+    countries = await countries_factory(instances=2)
+
+    vlogs = [
+        Vlog(
+            vlogger_id=vlogger.id,
+            country_id=countries[0].id,
+            youtube_video_id=f"videoid000{i}",
+            published_at=datetime.now(timezone.utc) + timedelta(days=i),
+            title=f"test_title_{i}",
+            thumbnail_url=f"test_thumbnail_{i}",
+            language="en",
+        )
+        for i in range(3)
+    ]
+    db_session.add_all(vlogs)
+    await db_session.commit()
+
+    response = await client.get(
+        f"/api/v2/vloggers/{vlogger.id}/country/{countries[0].id}?skip=0&limit=2&order=desc&language=en"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert len(data["vlogs"]) == 2
+
+    assert "published_at" in data["vlogs"][0]
+    assert data["vlogs"][0]["title"] == "test_title_2"
+    assert data["vlogs"][0]["thumbnail_url"] == "test_thumbnail_2"
+    assert data["vlogs"][0]["language"] == "en"
+    assert data["vlogs"][0]["vlogger_id"] == vlogger.id
+    assert data["vlogs"][0]["country_id"] == countries[0].id
+    assert data["vlogs"][0]["youtube_video_id"] == "videoid0002"
+    assert "created_at" in data["vlogs"][0]
+
+    assert data["vlogs"][1]["title"] == "test_title_1"
+    assert data["vlogs"][1]["thumbnail_url"] == "test_thumbnail_1"
+    assert data["vlogs"][1]["language"] == "en"
+    assert data["vlogs"][1]["vlogger_id"] == vlogger.id
+    assert data["vlogs"][1]["country_id"] == countries[0].id
+    assert data["vlogs"][1]["youtube_video_id"] == "videoid0001"
+    assert "created_at" in data["vlogs"][0]
+
+    assert data["skip"] == 0
+    assert data["limit"] == 2
+    assert data["has_more"] is True
+
+
+async def test_get_vlogger_vlogs_by_country_endpoint_invalid_vlogger(client):
+    response = await client.get("/api/v2/vloggers/99999/country/1")
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json()["detail"] == "Vlogger does not exist"
+
+
+async def test_get_vlogger_vlogs_by_country_endpoint_invalid_country(vlogger, client):
+    response = await client.get(f"/api/v2/vloggers/{vlogger.id}/country/99999")
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json()["detail"] == "Country does not exist"
+
+
+async def test_get_vlogger_vlogs_by_country_endpoint_invalid_type(client):
+    response = await client.get("/api/v2/vloggers/invalid/country/invalid")
 
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
     assert response.json()["detail"][0]["type"] == "int_parsing"
